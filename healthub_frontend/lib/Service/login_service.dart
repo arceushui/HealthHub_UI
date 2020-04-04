7950c52b706574fbbb41f8f16c5cc868feabc046
phangjunyu <phangjunyu@gmail.com>
2020-04-04 14:08:14 +0800

    before checkout

--- healthub_frontend/lib/Service/login_service.dart
+++ healthub_frontend/lib/Service/login_service.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 class LoginService {
   static const API = 'http://10.0.2.2:8080';
+
   static const headers = {'Content-Type': 'application/json'};
 
   Future<APIResponse<Object>> login(Login item) {
--- healthub_frontend/lib/WeightScreen.dart
+++ healthub_frontend/lib/WeightScreen.dart
@@ -1,4 +1,4 @@
-// import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:healthub_frontend/Widget/DrawerList.dart';
